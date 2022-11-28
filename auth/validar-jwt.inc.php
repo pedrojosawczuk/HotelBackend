d350bf4ab3943ae9fb54fe111f819bf886714786
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2022-11-28 17:59:31 -0300

    add: auth

---
+++ auth/validar-jwt.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+    require 'jwtutil.class.php';
+    require 'config.php';
+
+    $token = @getallheaders()['authorization'];
+
+    $responseBody = '';
+
+    if($token) {
+        $decoded = JwtUtil::decode($token, JWT_SECRET_KEY);
+    } else {
+        http_response_code(401); // Não autorizado
+        $responseBody = '{ "message": "Sem token" }';
+    }
+
+    // Se existir $responseBody == não está autorizado
+    if($responseBody) { // Mostrar a resposta para o cliente
+        $responseBody = '{ "message": "Token inválido" }';
+
+        // Defique que o conteúdo da resposta será um JSON (application/JSON)
+        header('Content-Type: application/json');
+    
+        // Exibe a resposta
+        print_r($responseBody);
+        exit; // Encerra o script
+    }
+?>
