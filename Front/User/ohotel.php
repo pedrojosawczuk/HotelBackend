6f2c109a34c56046cdfcd8cf7beb825ca0116eb7
Arthur Henrique Costa Nunes <arthur.art1048@gmail.com>
2022-12-02 03:40:06 -0300

    Front User

--- Front/User/ohotel.php
+++ Front/User/ohotel.php
@@ -7,7 +7,7 @@
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
-    <link rel="stylesheet" href="../styles/main.css">
+    <link rel="stylesheet" href="Front/styles/main.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
