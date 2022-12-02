f155a311375165bd1c5000da92446bbdcaf01692
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2022-12-02 02:40:12 -0300

    add: tarifa.class

---
+++ tarifa/tarifa.class.php
@@ -0,0 +1,9 @@
+<?php
+
+    class Tarifa {
+        function __construct($tipo_acomodacoes, $preco) {
+            $this -> tipo_acomodacoes = $tipo_acomodacoes;
+            $this -> preco = $preco;
+        }
+    }
+?>
